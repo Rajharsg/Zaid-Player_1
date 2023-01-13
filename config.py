78376cc6a85023c048a41bdb5a68fac7aa600340
Rajharsg <122608267+Rajharsg@users.noreply.github.com>
2023-01-13 22:29:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ if os.path.exists("local.env"):
 load_dotenv()
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME")
+SESSION_NAME = getenv("SESSION_NAME"," BQABBR_XKgy3Y0LKCe-KtJ_nGVIp2Kr7tcqE-DgfvEAFKYnEd-stt9kUY55UXg7Aqyk1r_3RZWILMenDYLtdnsXVXGX2A72gWvhI_6xm4XihLN9LIt2kwMsA7I3EqapL9CXpP5NQ00WxxXfT6o1VoS7COfHwLWC8Tjh236eO0bSgT0tJdeAsYlh5IrUEKm9pY7ZHaFBCwGfu0kfuwcOXnYvKNmV45Z0ozETh-35jUvhC3K4OuV0VmrYH1Fr37svO-flNuGA3pGBOiXIWiHaxKehwQWZYff3Sgn-f24aCFrIHevsr1iZqWNGXP_WKjzy8nRja1hRe4Xwx2ig2TWm8Ur8XAAAAAVSs9dAA")
 
 if str(getenv("STRING_SESSION2")).strip() == "":
     SESSION2 = str(None)
@@ -36,17 +36,17 @@ else:
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 BOT_NAME = getenv("BOT_NAME", "Umk")
 
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority")
-OWNER_NAME = getenv("OWNER_NAME", "Zaid")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Timesisnotwaiting")
-ALIVE_NAME = getenv("ALIVE_NAME", "Zaid")
-BOT_USERNAME = getenv("BOT_USERNAME", "Zaid2_Robot")
-OWNER_ID = getenv("OWNER_ID", "1669178360")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Zaid2_Assistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "TheSupportChat")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TheUpdatesChannel")
+API_ID = int(getenv("API_ID", "10181640"))
+API_HASH = getenv("API_HASH", "41d46ffe251fdf67035b24a05af628ec")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://vcbot:vcbot@cluster0.yqipgxg.mongodb.net/?retryWrites=true&w=majority")
+OWNER_NAME = getenv("OWNER_NAME", "harsh")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "harsh")
+ALIVE_NAME = getenv("ALIVE_NAME", "harsh")
+BOT_USERNAME = getenv("BOT_USERNAME", "Randi_musicbot")
+OWNER_ID = getenv("OWNER_ID", "5715588560")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "harsh_Assistant")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "alone_support")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "alone_support"")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "HEROKU_API_KEY")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
